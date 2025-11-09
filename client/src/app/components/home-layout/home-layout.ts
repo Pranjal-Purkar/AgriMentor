30fc94545a4ae559bca0d7bd046ac61e66d7dad3
sbhor007 <sbhor747@gmail.com>
2025-11-09 22:14:36 +0530

    configure Home layout

--- client/src/app/components/home-layout/home-layout.ts
+++ client/src/app/components/home-layout/home-layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { Home } from "../home/home";
 
 @Component({
   selector: 'app-home-layout',
-  imports: [],
+  imports: [Navbar, Home],
   templateUrl: './home-layout.html',
   styleUrl: './home-layout.scss',
 })

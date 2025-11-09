de1c1abb87582bba90f4f4f29681f26ab443c399
sbhor007 <sbhor747@gmail.com>
2025-11-09 21:58:49 +0530

    Configure and add home route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeLayout } from './components/home-layout/home-layout';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: HomeLayout,
+    children:[
+       {
+        path: 'home',
+        component: Home,
+      },
+    ]
+  },
+];

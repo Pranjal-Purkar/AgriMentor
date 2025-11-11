77b795f198df055f31aa3db19236bc320b5fd5db
sbhor007 <sbhor747@gmail.com>
2025-11-11 14:36:17 +0530

    desing home component

--- client/src/app/components/home-layout/home-layout.ts
+++ client/src/app/components/home-layout/home-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
+import { Home } from "../home/home";
 
 @Component({
   selector: 'app-home-layout',
-  imports: [Navbar],
+  imports: [Navbar, Home],
   templateUrl: './home-layout.html',
   styleUrl: './home-layout.css',
 })

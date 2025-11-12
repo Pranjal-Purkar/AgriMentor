91069469facf0793527af1da746148ecfadcf0b4
panjal purkar <purkarpranjal12@gmail.com>
2025-11-12 10:59:25 +0530

    .

--- server/src/main/java/com/server/service/UserService.java
+++ server/src/main/java/com/server/service/UserService.java
@@ -5,23 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.server.dto.RegisterRequest;
-import com.server.entity.Address;
-import com.server.entity.Farmer;
-import com.server.entity.User;
-import com.server.repository.AddressRepository;
-import com.server.repository.UserRepository;
+import com.server.entity.*;
+import com.server.repository.*;
+
 
 @Service
 public class UserService {
 	@Autowired
 	 private UserRepository userRepository;
-//	
-//	@Autowired
-//	private AddressRepository addressRepository;
-	
+
      public void registerUser(RegisterRequest request) {
-//    	 Address address = new Address("NashikRoad", "Nashik", "Mah", "422215", "India");
-//    	 addressRepository.save(address);
+// 
     	 System.out.println("User Service Test" + request.toString());
     	 User user = new User();
     	 user.setFirstName(request.getFirstName());

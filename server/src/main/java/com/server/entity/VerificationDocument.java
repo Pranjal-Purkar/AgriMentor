e6f83b523406356c31a166b7e8f4090b597aada2
sbhor007 <sbhor747@gmail.com>
2025-11-13 23:32:49 +0530

    create verification Document entity

---
+++ server/src/main/java/com/server/entity/VerificationDocument.java
@@ -0,0 +1,28 @@
+package com.server.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerificationDocument {
+		@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	private String documentType; // e.g., "ID Proof", "Qualification Certificate"
+
+	private String documentUrl; // URL or path to the stored document
+
+	@ManyToOne
+	@JoinColumn(name = "consultant_id")
+	private Consultant consultant;
+}

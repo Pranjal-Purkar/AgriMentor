4d1612f22045aadf20c8b3fce07fb165e393e575
sbhor007 <sbhor747@gmail.com>
2025-11-13 23:20:33 +0530

    create consultant request and response dto

---
+++ server/src/main/java/com/server/dto/ConsultantRegisterResponce.java
@@ -0,0 +1,26 @@
+package com.server.dto;
+
+import com.server.enumeration.Role;
+import com.server.enumeration.VerificationStatus;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ConsultantRegisterResponce {
+	private Long id;
+	private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Role role;
+    private String expertiseArea;
+    private int experienceYears;
+    private String qualifications;
+    private VerificationStatus verificationStatus;
+}

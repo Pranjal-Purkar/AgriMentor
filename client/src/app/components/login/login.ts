c7fc9305c23c508c188f7e35c48a995d806355d4
sbhor007 <sbhor747@gmail.com>
2025-11-13 14:49:50 +0530

    fix Role Switch tab UI

--- client/src/app/components/login/login.ts
+++ client/src/app/components/login/login.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass,NgStyle],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -23,12 +23,28 @@ export class Login {
   }
 
   setRole(role: string) {
-    this.activeRole = role;
+    if (this.activeRole !== role) {
+      this.activeRole = role;
+      // Add any additional logic you want to trigger on role change
+    }
   }
 
   get f() {
     return this.loginForm.controls;
   }
+
+  getResponsiveTranslate() {
+  const index = this.roles.indexOf(this.activeRole);
+
+  if (window.innerWidth < 640) {      // sm
+    return `translateX(${index * 310}%)`;
+  } else if (window.innerWidth < 1024) { // md
+    return `translateX(${index * 230}%)`;
+  } else {                           // lg
+    return `translateX(${index * 330}%)`;
+  }
+}
+
 
   onSubmit() {
     if (this.loginForm.invalid) {

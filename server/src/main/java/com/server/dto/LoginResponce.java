a66cefa682f3af81052e2f08cbb8fba58c10a99c
sbhor007 <sbhor747@gmail.com>
2025-11-23 18:30:10 +0530

    handle login request and response data

---
+++ server/src/main/java/com/server/dto/LoginResponce.java
@@ -0,0 +1,13 @@
+package com.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginResponce {
+	private String email;
+	private String jwt;
+}

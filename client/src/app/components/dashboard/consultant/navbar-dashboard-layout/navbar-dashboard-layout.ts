f948d7e26f3e910f0579b4af8015f96d2604aaa4
sbhor007 <sbhor747@gmail.com>
2025-11-23 15:43:32 +0530

    create Dashbord component

---
+++ client/src/app/components/dashboard/consultant/navbar-dashboard-layout/navbar-dashboard-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-dashboard-layout',
+  imports: [],
+  templateUrl: './navbar-dashboard-layout.html',
+  styleUrl: './navbar-dashboard-layout.css',
+})
+export class NavbarDashboardLayout {
+
+}

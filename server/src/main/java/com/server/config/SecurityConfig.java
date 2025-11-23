1e81486edeab9dfb2455ecd63f3f2ae8ff0fafca
sbhor007 <sbhor747@gmail.com>
2025-11-23 20:15:58 +0530

    implement security config:SecurityFilterChain, PasswordEncoder, AuthenticationManager

--- server/src/main/java/com/server/config/SecurityConfig.java
+++ server/src/main/java/com/server/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(
-				auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+				auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated())
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();

521086bc4d7929f42c4e29a63071d6f856b3c9bc
sbhor007 <sbhor747@gmail.com>
2025-11-23 18:02:23 +0530

    implement CustomeUserService:loadUserByUsername

--- server/src/main/java/com/server/repository/UserRepository.java
+++ server/src/main/java/com/server/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.server.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	void save(RegisterRequest request);
 	boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
+    Optional<User> findByEmail(String username);
 }

c1d921803809621670d8d615f94ce6e25943b95a
sbhor007 <sbhor747@gmail.com>
2025-11-24 08:05:33 +0530

    test and debug login controller

--- server/src/main/java/com/server/controller/UserController.java
+++ server/src/main/java/com/server/controller/UserController.java
@@ -2,6 +2,7 @@ package com.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.server.dto.RegisterRequest;
 import com.server.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/v1/users")
+@Slf4j
 public class UserController {
 	@Autowired
     private UserService userService;
 	
 	@GetMapping("/test")
-	public void Test() {
+	public void Test(Authentication authentication) {
+		log.info("User Controller Test accessed by: {}", authentication);
+		System.out.println(authentication.toString());
 			 	 System.out.println("User Controller Test");
 	}
 	

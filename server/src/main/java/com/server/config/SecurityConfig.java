2179cdee83214fc6be7c8c2e842f3f5d78245922
sbhor007 <sbhor747@gmail.com>
2025-11-28 01:26:47 +0530

    add remote CORS origin

--- server/src/main/java/com/server/config/SecurityConfig.java
+++ server/src/main/java/com/server/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig implements WebMvcConfigurer{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("http://localhost:4200","http://10.30.100.228:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
@@ -59,7 +59,7 @@ public class SecurityConfig implements WebMvcConfigurer{
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowedOrigins(List.of("http://localhost:4200"));
+	    config.setAllowedOrigins(List.of("http://localhost:4200","http://10.30.100.228:4200 "));
 	    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	    config.setAllowedHeaders(List.of("*"));
 	    config.setAllowCredentials(true);

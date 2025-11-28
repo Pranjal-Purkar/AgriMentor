9d8c0e48e214baf6f074ab056d279f486a84d718
sbhor007 <sbhor747@gmail.com>
2025-11-28 13:12:46 +0530

    Debug Component and nested ro

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,44 +8,44 @@ import { DashboardLayout } from './components/dashboards/dashboard-layout/dashbo
 import { FarmerDashboardLayout } from './components/dashboards/farmer/farmer-dashboard-layout/farmer-dashboard-layout';
 import { AdminDashboard } from './components/dashboards/admin/admin-dashboard/admin-dashboard';
 import { AdminDashboardLayout } from './components/dashboards/admin/admin-dashboard-layout/admin-dashboard-layout';
+import { Profile } from './components/dashboards/farmer/profile/profile';
+import { FarmerHome } from './components/dashboards/farmer/farmer-home/farmer-home';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeLayout,
     children: [
-      {
-        path: '',
-        component: Home,
-      },
-      {
-        path: 'register-farmer',
-        component: Farmer,
-      },
-      {
-        path: 'register-consultant',
-        component: Consultant,
-      },
-      {
-        path: 'login',
-        component: Login,
-      },
+      { path: '', component: Home },
+      { path: 'register-farmer', component: Farmer },
+      { path: 'register-consultant', component: Consultant },
+      { path: 'login', component: Login },
     ],
   },
+
+  // Dashboard Area
   {
     path: 'dashboard',
     component: DashboardLayout,
     children: [
       {
-        path: 'farmer-dashboard',
+        path: 'farmer',
         component: FarmerDashboardLayout,
+        children: [
+          { path: '', component: FarmerHome },       // /dashboard/farmer
+          { path: 'profile', component: Profile },  // /dashboard/farmer/profile
+        ],
       },
+
       {
-        path: 'admin-dashboard',
-        component: AdminDashboardLayout
+        path: 'admin',
+        component: AdminDashboardLayout,
+        children: [
+          { path: '', component: AdminDashboard }, // /dashboard/admin
+        ],
       }
     ],
-
   },
+
   { path: '**', redirectTo: '' },
 ];

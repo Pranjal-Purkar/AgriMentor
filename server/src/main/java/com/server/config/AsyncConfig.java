7ab48ca6c7399bbfe8b1cd73268cf9136635ed41
sbhor007 <sbhor747@gmail.com>
2025-11-29 23:32:40 +0530

    handle async task for Email

---
+++ server/src/main/java/com/server/config/AsyncConfig.java
@@ -0,0 +1,26 @@
+package com.server.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    @Bean(name = "emailExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("Async-OTP-");
+        executor.initialize();
+        return executor;
+    }
+
+
+}
--- server/src/main/java/com/server/service/EmailService.java
+++ server/src/main/java/com/server/service/EmailService.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
@@ -12,7 +15,8 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public boolean sendEmail(String to, String subject, String body) {
+    @Async("emailExecutor")
+    public CompletableFuture<Boolean> sendEmail(String to, String subject, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(to);
@@ -20,10 +24,58 @@ public class EmailService {
             message.setText(body);
             mailSender.send(message);
             log.info("Email sent successfully to {}", to);
-            return true;
+            return CompletableFuture.completedFuture(true);
         } catch (Exception e) {
             log.error("Failed to send email to {}", to, e);
-            return false;
+            return CompletableFuture.completedFuture(true);
+        }
+    }
+
+    @Async("emailExecutor")
+    public CompletableFuture<Boolean> sendEmail() {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo("sbhor372@gmail.com");
+            message.setSubject("Test Email");
+            message.setText("This is a test email from the AuthController.");
+            mailSender.send(message);
+            log.info("Email sent successfully to ");
+            return CompletableFuture.completedFuture(true);
+        } catch (Exception e) {
+            log.error("Failed to send email to ", e);
+            return CompletableFuture.completedFuture(true);
+        }
+    }
+
+    @Async("emailExecutor")
+    public CompletableFuture<Boolean> sendEmail( String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo("sbhor372@gmail.com");
+            message.setSubject("Admin Notification");
+            message.setText(body);
+            mailSender.send(message);
+            log.info("Email sent successfully to ");
+            return CompletableFuture.completedFuture(true);
+        } catch (Exception e) {
+            log.error("Failed to send email to ", e);
+            return CompletableFuture.completedFuture(true);
+        }
+    }
+
+    @Async("emailExecutor")
+    public CompletableFuture<Boolean> sendEmail(String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo("sbhor372@gmail.com");
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+            log.info("Email sent successfully to {}", "");
+            return CompletableFuture.completedFuture(true);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}", "", e);
+            return CompletableFuture.completedFuture(true);
         }
     }
 }
